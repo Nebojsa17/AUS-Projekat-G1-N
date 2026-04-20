33e17b98db9e57a0c7e6256f71749ab4dbef4ca1
Nebojsa <golusin.pr17.2023@uns.ac.rs>
2026-04-20 18:44:23 +0200

    Implementirao ReadCoils i ReadHoldingRegisters Functions.

--- dCom/Modbus/ModbusFunctions/ReadCoilsFunction.cs
+++ dCom/Modbus/ModbusFunctions/ReadCoilsFunction.cs
@@ -25,14 +25,62 @@ namespace Modbus.ModbusFunctions
         public override byte[] PackRequest()
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            byte[] retArray = new byte[12];
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.TransactionId)), 0, retArray, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.ProtocolId)), 0, retArray, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.Length)), 0, retArray, 4, 2);
+
+            retArray[6] = CommandParameters.UnitId;
+            retArray[7] = CommandParameters.FunctionCode;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)((ModbusReadCommandParameters)CommandParameters).StartAddress)), 0, retArray, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)((ModbusReadCommandParameters)CommandParameters).Quantity)), 0, retArray, 10, 2);
+
+            return retArray;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            Dictionary<Tuple<PointType, ushort>, ushort> ret = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            if ((response[7] & 0x80) != 0)
+            {
+                HandeException(response[8]);
+                return ret;
+            }
+
+            int bytCnt = response[8];
+            int rest = ((ModbusReadCommandParameters)CommandParameters).Quantity;
+            ushort addr = ((ModbusReadCommandParameters)CommandParameters).StartAddress;
+
+            int bytIndx = 0;
+
+            while(bytIndx < bytCnt && rest > 0) 
+            {
+                byte currByt = response[9 + bytIndx];
+
+                int bitIndx = 0;
+                while(bitIndx < 8 && rest > 0) 
+                {
+                    ushort value = (ushort)(currByt & 0x01);
+
+                    var key = new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT,addr);
+
+                    ret[key] = value;
+
+                    currByt >>= 1;
+                    addr++;
+                    bitIndx++;
+                    rest--;
+                }
+
+                bytIndx++;
+            }
+
+            return ret;
         }
     }
 }
--- dCom/Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
+++ dCom/Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
@@ -25,14 +25,50 @@ namespace Modbus.ModbusFunctions
         public override byte[] PackRequest()
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            byte[] retArray = new byte[12];
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.TransactionId)), 0, retArray, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.ProtocolId)), 0, retArray, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)CommandParameters.Length)), 0, retArray, 4, 2);
+
+            retArray[6] = CommandParameters.UnitId;
+            retArray[7] = CommandParameters.FunctionCode;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)((ModbusReadCommandParameters)CommandParameters).StartAddress)), 0, retArray, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)((ModbusReadCommandParameters)CommandParameters).Quantity)), 0, retArray, 10, 2);
+
+            return retArray;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            Dictionary<Tuple<PointType, ushort>, ushort> ret = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            if ((response[7] & 0x80) != 0)
+            {
+                HandeException(response[8]);
+                return ret;
+            }
+
+            int bytCnt = response[8];
+            ushort addr = ((ModbusReadCommandParameters)CommandParameters).StartAddress;
+
+            int registers = bytCnt / 2;
+
+            for (int i = 0; i < registers; i++)
+            {
+                int pos = 9 + (i * 2);
+
+                ushort value = (ushort)((response[pos] << 8) | response[pos + 1]);
+
+                var key = new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, (ushort)(addr + i));
+
+                ret[key] = value;
+            }
+
+            return ret;
         }
     }
 }
